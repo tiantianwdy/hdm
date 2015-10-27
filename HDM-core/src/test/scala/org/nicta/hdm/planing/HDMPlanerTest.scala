26222e6b3fe8f3eb8c5575871dd026eb68a45c9d
Dongyao Wu <tiantianwdy@163.com>
2015-10-27 18:29:32 +1100

    updated changes for reduce operation in testing

--- HDM-core/src/test/scala/org/nicta/hdm/planing/HDMPlanerTest.scala
+++ HDM-core/src/test/scala/org/nicta/hdm/planing/HDMPlanerTest.scala
@@ -36,7 +36,7 @@ class HDMPlanerTest {
     HDMContext.init()
     val hdm = HDM.horizontal(text, text2)
     val wordCount = hdm.map(d=> (d,1))
-      .groupBy(_._1).map(t => (t._1, t._2.map(_._2))).reduce(("", Buf(0)))((t1,t2) => (t1._1, t1._2))
+      .groupBy(_._1).map(t => (t._1, t._2.map(_._2))).reduce((t1,t2) => (t1._1, t1._2))
     val wordCountOpti = new FunctionFusion().optimize(wordCount)
     val hdms = StaticPlaner.plan(wordCountOpti ,2)
     hdms.foreach(println(_))
