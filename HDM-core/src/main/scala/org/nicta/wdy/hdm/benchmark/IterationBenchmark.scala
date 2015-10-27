70a28e7ce6c9aa2efde47d45db854f64c3fa16ad
Dongyao Wu <tiantianwdy@163.com>
2015-10-27 18:28:18 +1100

    added regression  benchmark for testing iterations

--- HDM-core/src/main/scala/org/nicta/wdy/hdm/benchmark/IterationBenchmark.scala
+++ HDM-core/src/main/scala/org/nicta/wdy/hdm/benchmark/IterationBenchmark.scala
@@ -1,8 +1,12 @@
 package org.nicta.wdy.hdm.benchmark
 
+import breeze.linalg.{Vector, DenseVector}
 import org.nicta.wdy.hdm.executor.HDMContext
 import org.nicta.wdy.hdm.io.Path
 import org.nicta.wdy.hdm.model.HDM
+
+import scala.math._
+import scala.util.Random
 
 /**
  * Created by tiantian on 8/10/15.
@@ -77,6 +81,36 @@ class IterationBenchmark(val kIndex:Int = 0, val vIndex:Int = 1)  extends Serial
       Thread.sleep(100)
     }
 
+  }
+
+  case class DataPoint(x: Vector[Double], y: Double) extends Serializable
+
+  def testLogisticRegression(dataPath:String, iterations:Int, p:Int = 4) = {
+    implicit  val parallelism = p
+    val path = Path(dataPath)
+    val kOffset = kIndex
+    val vOffset = vIndex
+    val rand = new Random(42)
+    var weights = DenseVector.fill(2){2 * rand.nextDouble - 1}
+
+    val hdm = HDM(path).map{ w =>
+      val as = w.split(",")
+      val arr = Array(as(kOffset), as(vOffset)).map(_.toDouble)
+      DataPoint(Vector(arr), arr(0))
+    }.cache
+
+
+    for(i <- 1 to iterations) {
+      val start = System.currentTimeMillis()
+      val w = weights
+//      val redFunc = (d1:Vector[Double], d2: Vector[Double]) => d1 + d2
+      val gradient = hdm.map{ p =>
+        p.x * (1 / (1 + exp(-p.y * (w.dot(p.x)))) - 1) * p.y
+      }.reduce(_ + _).collect().next()
+      weights -= gradient
+      val end = System.currentTimeMillis()
+      println(s"Time consumed for iteration $i : ${end - start} ms. weights: $weights")
+    }
   }
 
 
