87a104f7da4ea13ae4d25aec860478a7cf45b2fa
Dongyao Wu <tiantianwdy@163.com>
2015-11-26 19:55:06 +1100

    refractor aggregatable interface for functions

--- HDM-core/src/main/scala/org/nicta/wdy/hdm/functions/SerializableFunction.scala
+++ HDM-core/src/main/scala/org/nicta/wdy/hdm/functions/SerializableFunction.scala
@@ -14,25 +14,25 @@ trait SerializableFunction[I,R] extends  Serializable{
 
   def apply(params: I) : R
 
-  //  def andThen[U](func: SerializableFunction[R,U]): SerializableFunction[I, U] = ???
 }
 
-trait Aggregatable[T,R] {
+trait Aggregatable[T,R] extends  Serializable {
 
-  def zero(params:T): R
 
   def aggregate(params:T, res:R): R
 
 
 }
 
-trait ShuffleAggregatable[K,T,R] extends Aggregatable[T,R]{
+trait ShuffleAggregatable[K, T, R] extends Aggregatable[T,R]{
 
-  def map(param:T):K
+  def zero(params:T): R
+
+  def map(param:T): K
 
 }
 
-trait Mergeable[R] {
+trait Mergeable[R]{
 
   def merge(params:Seq[R], res:Buffer[R]): Buffer[R]
 
