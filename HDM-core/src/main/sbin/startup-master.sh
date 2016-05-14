785a5e6bf39eff9791f2762a4ead327c9ce18deb
Dongyao Wu <tiantianwdy@163.com>
2016-05-14 20:14:02 +1000

    updated starting scripts to support different cluster mode

--- HDM-core/src/main/sbin/startup-master.sh
+++ HDM-core/src/main/sbin/startup-master.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8100"
+# DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8100"
 business_path=./
 cd $business_path
 lib=`find lib -name *.jar | xargs`
-java $DEBUG_OPTS -Dfile.encoding=UTF-8 -cp "$lib" -jar ./HDM-core-0.0.1.jar -m true -p 8999
+port="8999"
+mode="single-cluster"
+if [ $# -gt 1 ]; then
+ port="$1"
+fi
+if [ $# -gt 2 ]; then
+ mode="$2"
+fi
+
+java $DEBUG_OPTS -Dfile.encoding=UTF-8 -cp "$lib" -jar ./HDM-core-0.0.1.jar -m true -p $port -M $mode
