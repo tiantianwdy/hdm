5604a8454ce24caa81d95ff93952b8b7c2dad53b
Dongyao Wu <tiantianwdy@163.com>
2016-05-16 20:50:54 +1000

    Add NotifyLock to better support wait and notify in scala

---
+++ HDM-core/src/main/java/org/nicta/wdy/hdm/utils/NotifyLock.java
@@ -0,0 +1,34 @@
+package org.nicta.wdy.hdm.utils;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by tiantian on 16/05/16.
+ */
+public class NotifyLock {
+
+    private  Integer lock = 0;
+
+    private AtomicBoolean valid = new AtomicBoolean(false);
+
+
+    public void acquire() throws InterruptedException{
+        synchronized(lock) {
+            valid.set(false);
+            lock.wait();
+        }
+    }
+
+    public void release(){
+        synchronized(lock)  {
+            lock.notifyAll();
+            valid.set(true);
+        }
+    }
+
+    public Boolean isAvailable(){
+        return valid.get();
+    }
+
+
+}
