76b2ba1a6cab6ebee02358ae12ee733e6cd54682
Dongyao Wu <tiantianwdy@163.com>
2016-06-28 15:17:27 +1000

    add Hungarian algorithm for scheduling and test.

--- HDM-core/src/test/scala/org/nicta/hdm/scheduling/SchedulingPolicyTest.scala
+++ HDM-core/src/test/scala/org/nicta/hdm/scheduling/SchedulingPolicyTest.scala
@@ -35,7 +35,10 @@ class SchedulingPolicyTest extends SchedulingTestData {
 
   def getSchedulingReport(schedulingPolicy:SchedulingPolicy, tasks:Seq[SchedulingTask], resources:Seq[Path], computeFactor: Float, ioFactor: Float, networkFactor:Float): Unit ={
     val taskMap = tasks.map(t => t.id -> t).toMap
+    val startTime = System.currentTimeMillis()
     val comparedJobs = schedulingPolicy.plan(tasks, resources, computeFactor, ioFactor, networkFactor)
+    val endTime = System.currentTimeMillis()
+    println(s"time taken for scheduling: ${endTime - startTime} ms.")
     val exeTimeOfTasks = comparedJobs.groupBy(_._2).map { kSeq => // (resourceId, taskList)
       kSeq._1 -> kSeq._2.map { kv => // (taskId, resourceId)
         val t = taskMap.get(kv._1).get
@@ -65,28 +68,31 @@ class SchedulingPolicyTest extends SchedulingTestData {
 
   @Test
   def testSchedulingComparison: Unit ={
-    val numTask = 48
-    val inputEachTask = 8
+    val numTask = 1000
+    val inputEachTask = 1
     val tasks = generateTasks(numTask, inputEachTask)
-    val resources = generateResources(8)
+    val resources = generateResources(160)
 
-    val cpuFactor = 5F
-    val ioFactor = 10F
-    val networkFactor = 15F
+    val cpuFactor = 1F
+    val ioFactor = 5F
+    val networkFactor = 10F
     println("================ min-min scheduling =======================")
     val schedulingPolicy = new MinMinScheduling
     getSchedulingReport(schedulingPolicy,tasks, resources, cpuFactor, ioFactor, networkFactor)
 
-    // compare with max-min scheduling
-    println("================ max-min scheduling =======================")
-    val maxMinScheduling = new MaxMinScheduling
-    getSchedulingReport(maxMinScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
-    // compare with fair-scheduling
-    println("================ fair-scheduling =======================")
-    getSchedulingReport(new FairScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
+//    // compare with max-min scheduling
+//    println("================ max-min scheduling =======================")
+//    val maxMinScheduling = new MaxMinScheduling
+//    getSchedulingReport(maxMinScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
+//    // compare with fair-scheduling
+//    println("================ fair-scheduling =======================")
+//    getSchedulingReport(new FairScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
 
     println("================ Simple-scheduling =======================")
     getSchedulingReport(new OneByOneScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
+
+    println("================ Hungraian Scheduling =======================")
+    getSchedulingReport(new HungarianScheduling, tasks, resources, cpuFactor, ioFactor, networkFactor)
   }
 
 }
