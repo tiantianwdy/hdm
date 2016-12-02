bdffd35ecb9f89043e9a49d407d03238298adbb1
Dongyao Wu <tiantianwdy@163.com>
2016-12-02 17:47:57 +1100

    Add JSON data parser

--- HDM-core/src/main/scala/org/nicta/wdy/hdm/io/Serializer.scala
+++ HDM-core/src/main/scala/org/nicta/wdy/hdm/io/Serializer.scala
@@ -4,9 +4,16 @@ import java.io._
 import java.nio.charset.Charset
 
 import akka.serialization.Serializer
+import org.codehaus.jackson.map.{SerializationConfig, ObjectMapper}
+import org.codehaus.jackson.util.DefaultPrettyPrinter
+import org.codehaus.jackson.{JsonEncoding, JsonFactory}
+import org.codehaus.jackson.JsonParser.Feature
 import org.nicta.wdy.hdm.Arr
+import sun.org.mozilla.javascript.json.JsonParser
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.reflect.ClassTag
+import scala.reflect._
 
 /**
  * Created by tiantian on 25/12/14.
@@ -82,4 +89,95 @@ class FileLineIterator(private val reader: BufferedReader) extends Iterator[Stri
     nextLine = reader.readLine
     next
   }
+}
+
+class JsonObjectSerializer[T:ClassTag] extends BlockSerializer[T] {
+
+  val mapper = JacksonUtils.objectMapper
+
+  val jsonFactory = JacksonUtils.jsonFactory
+
+
+
+  override def identifier: Int = 10
+
+  override def includeManifest: Boolean = false
+
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): T = {
+    mapper.reader().readValue(bytes)
+  }
+
+  override def fromInputStream(in: InputStream): Seq[T] = {
+    val objs = mapper.reader(classTag[T].runtimeClass).readValues(in)
+    val buf = ArrayBuffer.empty[T]
+    while(objs.hasNext){
+      buf += objs.next()
+    }
+    buf
+  }
+
+  override def toBinary(o: T): Array[Byte] = {
+    mapper.writer().writeValueAsBytes(o)
+  }
+
+  override def toOutputStream(lst: Seq[T], out: OutputStream): Unit = {
+    val jsonGenerator = jsonFactory.createJsonGenerator(out, JsonEncoding.UTF8)
+    mapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false)
+    jsonGenerator.setCodec(mapper)
+    jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter)
+    jsonGenerator.writeStartArray()
+    lst.foreach{ elem =>
+      jsonGenerator.writeObject(elem)
+    }
+    jsonGenerator.writeEndArray()
+    jsonGenerator.flush()
+    jsonGenerator.close()
+  }
+
+  override def iteratorInputStream(in: InputStream): Iterator[T] = {
+    new JsonReaderIterator(in)
+  }
+
+  /**
+   *
+   * @param in
+   * @tparam U
+   */
+  class JsonReaderIterator[U](in: InputStream) extends Iterator[U]{
+
+    val objs = mapper.reader().readValues[U](in)
+
+    override def hasNext: Boolean = {
+      objs.hasNext
+    }
+
+    override def next(): U = {
+     objs.next()
+    }
+  }
+
+}
+
+
+
+object JacksonUtils {
+
+  val jsonFactory = new JsonFactory()
+  jsonFactory.configure(Feature.AUTO_CLOSE_SOURCE, false)
+  lazy val objectMapper = new ObjectMapper(jsonFactory)
+
+
+  def getJSONParser(is: InputStream) = {
+    val jsonParser = jsonFactory.createJsonParser(is)
+    jsonParser
+  }
+
+
+  def getJSONWriter(os:OutputStream) ={
+    val jsonWriter = jsonFactory.createJsonGenerator(os, JsonEncoding.UTF8)
+    jsonWriter
+  }
+
+
+
 }
